67dca4baf89ae49177473b910911d46165ad1d07
elliott-fogg <elliott.fogg@gmail.com>
2019-01-29 17:26:08 +0000

    Started on data visualisation structures

---
+++ datastruct_class.py
@@ -0,0 +1,18 @@
+# The data structure used to hold the gymchecker information for analysis
+
+# Will be initialised once, and all data will be loaded into it at that time.
+
+#
+
+class dataStruct(object):
+    def __init__(self, data):
+        pass
+
+class day():
+    # Contains data for an entire day, and what day it is
+    pass
+
+class week():
+    # Contains 'day' objects for an entire week, and what week of the year it is
+    # NOTE: The weeks are denoted by their position relative to student terms.
+    pass
